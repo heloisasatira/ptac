d03457b365e2dba25e16f3e34b1adf9ffeab781a
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2024-06-19 17:03:25 -0300

    Update Main.js

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,31 +1,59 @@
-'use server'
+"use client"
+import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
 import styles from "./main.module.css";
 
-export default async function Main() {
-/*     const [produtos, setProdutos] = useState([]);
+export default function Main() {
+  const [listProduct, setProduct] = useState([]);
 
     useEffect(() => {
-        const fetch = async () => {
+        const getProduct = async () => {
           const response = await fetch("https://fakestoreapi.com/products");
           const data = await response.json();
-            setProdutos(data);
-        };
-        fetch();
-      }, []); */
+          setProduct(data);
+        }
+        getProduct();
+      }, []);
 
-      const response = await fetch("https://fakestoreapi.com/products");
-      const produtos = await response.json();
-    
+      const orderAz = ()=> {
+        const listaAuxiliar = [...listProduct].sort((a, b) => a.title.localeCompare(b.title));
+        setProduct(listaAuxiliar);
+      }
+
+      const orderZa = ()=> {
+        let listaAuxiliar = [...listProduct].reverse((a, b) => a.title.localeCompare(b.title));
+        setProduct(listaAuxiliar);
+      }
+
+      const orderCres = ()=> {
+        let listaAuxiliar = [...listProduct].sort((a, b) => (b.price - a.price) );
+        setProduct(listaAuxiliar);
+      }
+
+      const orderDrec = ()=> {
+        let listaAuxiliar = [...listProduct].reverse((a, b) => (b.price - a.price));
+        setProduct(listaAuxiliar);
+      }
+
+      if(listProduct[0] == null){
+        return <Spinner/>
+      }
+
       return (
+        <>
+        <div className={styles.filters}>
+          <button onClick={ orderAz }>Az</button>
+          <button onClick={ orderZa }>Za</button>
+          <button onClick={ orderCres }>crescente</button>
+          <button onClick={ orderDrec }>decrescente</button>
+        </div>
    <main className={styles.main}> 
-
         {/* <div class="row" className={styles.row}>
         <div class="column" className={styles.column}> */}
-        {produtos.map((produto) => (
-
+        {listProduct.map((produto) => (
         <div class="card" className={styles.card} key={produto. id}>
           <br/>
-
+        
         <img src={produto. image} className={styles.image} alt={produto.title} />
 
         <h3 className={styles.paragrafo}>{produto.title}</h3>
@@ -34,11 +62,11 @@ export default async function Main() {
         <p className={styles.paragrafo}>Price: ${produto.price}</p>
         <p className={styles.paragrafo}>Rating: {produto.rating.count}stars</p>
 
-        <button className={styles.button} class="button">Adicionar</button>
-
+        {/* <button className={styles.button} class="button">Adicionar</button> */}
         </div>
         
         ))}
        </main>
+       </>
            );
     }; 
